cb79c44e16b2c03d14880be51372f0c241d6e1ba
jakob <jakob.willforss@hotmail.com>
2016-10-23 17:57:07 +0200

    Full SVM exercise

---
+++ c2_svm/full_svm_exercise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+import sklearn.svm
+import sklearn.metrics
+
+# Get preprocessed data
+features_train, features_test, labels_train, labels_test = preprocess()
+
+# Prepare test parameter range (here: parameter C)
+param_c_values = [1, 10, 100, 1000, 10000]
+
+for c_value in param_c_values:
+
+    clf = sklearn.svm.SVC(kernel="rbf", C=c_value)
+    # clf = SVC(kernel="linear")
+
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print("Training time: {}".format(time() - t0))
+
+    pred = clf.predict(features_test)
+
+    accuracy = sklearn.metrics.accuracy_score(labels_test, pred)
+
+    print("Accuracy: {}".format(accuracy))
+
+
+
+
--- c2_svm/svm_with_accuracy.py
+++ c2_svm/svm_with_accuracy.py
@@ -7,11 +7,17 @@ import copy
 import numpy as np
 import pylab as pl
 
+import time
+
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 from sklearn.svm import SVC
 clf = SVC(kernel="linear")
+
+t0 = time.time()
 clf.fit(features_train, labels_train)
+print("Training time: {}".format(time() - t0))
+
 pred = clf.predict(features_test)
 
 from sklearn.metrics import accuracy_score
